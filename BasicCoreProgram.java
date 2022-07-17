fefa3d45760cb8d1f1573536ec7eb7bf6bce8867
Harish mahur <hmahur20@gmail.com>
2022-07-17 21:54:44 +0530

    welcome to basic programming

---
+++ BasicCoreProgram.java
@@ -0,0 +1,11 @@
+package com.basic.programm;
+
+public class BasicCoreProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to basic programming");
+
+	}
+
+}

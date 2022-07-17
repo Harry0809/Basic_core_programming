33452077693c1394dcd00b88d9c622ab98325f2f
Harish mahur <hmahur20@gmail.com>
2022-07-17 22:45:25 +0530

    modified:power of 2

---
+++ _BasicCoreProgram.java
@@ -0,0 +1,27 @@
+package com.basic.programm;
+
+import java.util.Scanner;
+
+public class _BasicCoreProgram {
+	public static void main(String[] args) {
+		int num;
+		int mul = 1;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Power Value N:");
+		num = sc.nextInt();
+		if (num > 31) {
+			System.out.println("value of number should be less than 31");
+			return;
+		}
+
+		else {
+			System.out.println(mul);
+			for (int j = 1; j <= num; j++) {
+				mul = mul * 2;
+				System.out.println(mul);
+			}
+		}
+
+	}
+}

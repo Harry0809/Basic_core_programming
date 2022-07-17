f85494d3d46ab500bd9fb06a512ca850c44ad5d8
Harish mahur <hmahur20@gmail.com>
2022-07-17 22:53:27 +0530

    modified: harmonic number

--- _BasicCoreProgram.java
+++ _BasicCoreProgram.java
@@ -4,24 +4,19 @@ import java.util.Scanner;
 
 public class _BasicCoreProgram {
 	public static void main(String[] args) {
-		int num;
-		int mul = 1;
 
+		double result = 0, num = 0, i;
+		int n;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Power Value N:");
-		num = sc.nextInt();
-		if (num > 31) {
-			System.out.println("value of number should be less than 31");
-			return;
-		}
 
-		else {
-			System.out.println(mul);
-			for (int j = 1; j <= num; j++) {
-				mul = mul * 2;
-				System.out.println(mul);
-			}
+		System.out.print("enter the harmonic you want to find: ");
+		n = sc.nextInt();
+
+		for (i = 1; i <= n; i++) {
+			num = num + (1 / i);
+			result = num;
 		}
+		System.out.println("the value of harmonic no. " + n + " is: " + result);
 
 	}
 }

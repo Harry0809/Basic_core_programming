6c370e480e94c2421741a9de6eb3bd67efbef44e
Harish mahur <hmahur20@gmail.com>
2022-07-18 10:16:27 +0530

    modified: number is even or odd

--- _BasicCoreProgram.java
+++ _BasicCoreProgram.java
@@ -6,15 +6,12 @@ public class _BasicCoreProgram {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number for X");
-		int X = sc.nextInt();
-		System.out.println("Enter the number for Y");
-		int Y = sc.nextInt();
-		System.out.println("Before Swapping X = " + X + ", Y = " + Y);
-		int Z = X;
-		X = Y;
-		Y = Z;
-		System.out.println("After Swapping X = " + X + ", Y = " + Y);
-	}
+		System.out.println("Enter the Number");
+		int Number = sc.nextInt();
+		if (Number % 2 == 0) {
+			System.out.println(Number + " " + "is an Even Number");
+		} else
+			System.out.println(Number + " " + "is an Even Number");
 
+	}
 }

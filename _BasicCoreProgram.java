c1d8b6e63d0968a22ee6b67e9954b5600c4831c0
Harish mahur <hmahur20@gmail.com>
2022-07-18 22:06:25 +0530

    modified: quadratic

--- _BasicCoreProgram.java
+++ _BasicCoreProgram.java
@@ -1,17 +1,39 @@
 package com.basic.programm;
 
 import java.util.Scanner;
-
 public class _BasicCoreProgram {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the point x");
-		int x = sc.nextInt();
-		System.out.println("Enter the point y");
-		int y = sc.nextInt();
-		Double distance = (Double) Math.sqrt(Math.pow(x, 2) + Math.pow(x, 2));
-		System.out.println("Euclidean distance from the point (x, y) to the origin (0, 0) :" + distance);
+	
+	Scanner sc = new Scanner(System.in);
+	public void input(){
+		System.out.println("Enter the value for a");
+		int a = sc.nextInt();
+		System.out.println("Enter the value for b");
+		int b = sc.nextInt();
+		System.out.println("Enter the value for c");
+		int c = sc.nextInt();
+		System.out.  println("find the roots of the equation "+a+"*x*x + "+b+"x + "+c);
+		computedelta(a,b,c);
+		}
+    
+	public void computedelta(int a, int b,int c) {
+		int delta = b * b - 4 * a *c;
+	   System.out.println(delta);
+	   
+	   if(delta > 0 ) {
+		   double x1 = (-b + Math.sqrt(delta))/(2*a);
+		   double x2 = (-b - Math.sqrt(delta))/(2*a);
+	          System.out.println("Root 1 of x :"+x1);
+	          System.out.println("Root 2 of x :"+x2);
+	   }else if(delta == 0) {
+		      double x1 = -b / (2.0 * a);
+              System.out.println("The root is " + x1);   
+	   }else {
+		      System.out.println("Roots are not real.");
+	   }
 	}
 
+	public static void main(String[] args) {
+		_BasicCoreProgram obj = new  _BasicCoreProgram();
+		 obj.input();
+	}
 }

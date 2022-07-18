ed77155804c4a4191db5509afb739509db6c962b
Harish mahur <hmahur20@gmail.com>
2022-07-18 10:39:23 +0530

    modified: largest number

--- _BasicCoreProgram.java
+++ _BasicCoreProgram.java
@@ -3,25 +3,28 @@ package com.basic.programm;
 import java.util.Scanner;
 
 public class _BasicCoreProgram {
+
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a letter");
-		char X = sc.next().charAt(0);
-		int i = 0;
-		int flag = 0;
-		char volwel[] = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+		_BasicCoreProgram num = new _BasicCoreProgram();
+		System.out.println("Enter a number");
+		int X = sc.nextInt();
+		System.out.println("Enter a number");
+		int Y = sc.nextInt();
+		System.out.println("Enter a number");
+		int Z = sc.nextInt();
+		num.maxinum(X, Y, Z);
 
-		while (i < volwel.length) {
-			if (volwel[i] == X) {
-				flag++;
-			}
-			i++;
-			if (flag != 0) {
-				System.out.println("Alphabet is volwel ");
-			} else {
-				System.out.println("Alphabet is consonant ");
-			}
+	}
+
+	public void maxinum(int x, int y, int z) {
+		if (x > y && x > z) {
+			System.out.println(x + " is a Largest Number");
+		} else if (y > x && y > z) {
+			System.out.println(y + " is a Largest Number");
+		} else {
+			System.out.println(z + " is a Largest Number");
 		}
 	}
+
 }

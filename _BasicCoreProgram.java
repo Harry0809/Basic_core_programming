737cd04b9a7061edb5f0375f80986b0babe6b2eb
Harish mahur <hmahur20@gmail.com>
2022-07-18 21:56:04 +0530

    modified: distance

--- _BasicCoreProgram.java
+++ _BasicCoreProgram.java
@@ -1,37 +1,17 @@
 package com.basic.programm;
 
+import java.util.Scanner;
+
 public class _BasicCoreProgram {
 
 	public static void main(String[] args) {
-
-		int arr[] = { 0, -1, 2, -3, 1 };
-		int n = arr.length;
-		findTriplets(arr , n);
-	
-	class Num {
-
-		void findTriplets(int arr[], int n) {
-			boolean check = false;
-			for (int i = 0; i < n - 2; i++) {
-				for (int j = i + 1; j < n - 1; j++) {
-					for (int k = j + 1; k < n; k++) {
-						if (arr[i] + arr[j] + arr[k] == 0) {
-							System.out.print(arr[i]);
-							System.out.print(" ");
-							System.out.print(arr[j]);
-							System.out.print(" ");
-							System.out.print(arr[k]);
-							System.out.print("");
-							check = true;
-						}
-					}
-				}
-			}
-
-			if (check == false)
-				System.out.println(" not exist ");
-
-		}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the point x");
+		int x = sc.nextInt();
+		System.out.println("Enter the point y");
+		int y = sc.nextInt();
+		Double distance = (Double) Math.sqrt(Math.pow(x, 2) + Math.pow(x, 2));
+		System.out.println("Euclidean distance from the point (x, y) to the origin (0, 0) :" + distance);
 	}
-  }
+
 }

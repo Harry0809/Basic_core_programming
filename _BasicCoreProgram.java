da5ffb07b38218071b73f0f1403eea3caad1462b
Harish mahur <hmahur20@gmail.com>
2022-07-18 10:09:43 +0530

    modified: swapping of two number

--- _BasicCoreProgram.java
+++ _BasicCoreProgram.java
@@ -6,15 +6,15 @@ public class _BasicCoreProgram {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter th Dividened");
-		int number1 = sc.nextInt();
-		System.out.println("Enter the Divisor");
-		int number2 = sc.nextInt();
-		double quotient = (double) number1 / number2;
-		System.out.println("Quotient :" + quotient);
-		int remainder = number1 % number2;
-		System.out.println("Remainder :" + remainder);
-
+		System.out.println("Enter the number for X");
+		int X = sc.nextInt();
+		System.out.println("Enter the number for Y");
+		int Y = sc.nextInt();
+		System.out.println("Before Swapping X = " + X + ", Y = " + Y);
+		int Z = X;
+		X = Y;
+		Y = Z;
+		System.out.println("After Swapping X = " + X + ", Y = " + Y);
 	}
 
 }

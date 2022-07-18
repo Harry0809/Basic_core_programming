45a21852b555e93e4e3ec905d2393e03308931e6
Harish mahur <hmahur20@gmail.com>
2022-07-18 10:31:49 +0530

    modified: letter is consonant or vowel

--- _BasicCoreProgram.java
+++ _BasicCoreProgram.java
@@ -3,15 +3,25 @@ package com.basic.programm;
 import java.util.Scanner;
 
 public class _BasicCoreProgram {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Number");
-		int Number = sc.nextInt();
-		if (Number % 2 == 0) {
-			System.out.println(Number + " " + "is an Even Number");
-		} else
-			System.out.println(Number + " " + "is an Even Number");
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a letter");
+		char X = sc.next().charAt(0);
+		int i = 0;
+		int flag = 0;
+		char volwel[] = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+		while (i < volwel.length) {
+			if (volwel[i] == X) {
+				flag++;
+			}
+			i++;
+			if (flag != 0) {
+				System.out.println("Alphabet is volwel ");
+			} else {
+				System.out.println("Alphabet is consonant ");
+			}
+		}
 	}
 }
